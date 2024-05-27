83b033aa8e5d21f350a5caa6f4b8278e84002913
emily1120 <168400249+emily1120@users.noreply.github.com>
2024-05-27 14:24:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(msg)+Q)
 
 # 定義字典
 words_dict = {
