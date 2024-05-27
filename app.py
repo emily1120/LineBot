a1ee193801a14c60544614ce81698f3351f0085a
emily1120 <168400249+emily1120@users.noreply.github.com>
2024-05-27 14:21:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,9 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = Q
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(A))
+    msg = event.message.text
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
+
 # 定義字典
 words_dict = {
     "你今天來這裡的原因是什麼？": "我感到工作和個人責任讓我不堪重負。",
@@ -73,7 +74,6 @@ if Q == 'quit':
     break
 A=words_dict[Q]
 print("答案:",A )
-line_bot_api.reply_message(event.reply_token, TextSendMessage(A))
 
 @handler.add(PostbackEvent)
 def handle_message(event):
